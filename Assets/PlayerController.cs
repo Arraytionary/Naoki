a03a46baf5c6a3924fbcfac8e124890226ef8dfc
Arraytionary <31273055+Arraytionary@users.noreply.github.com>
2019-07-02 20:44:01 +0700

    2 player

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,23 +6,32 @@ using System;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] float startAngle;
+    [SerializeField] string horizontal;
+    [SerializeField] string vertical;
     private float h;
     private float v;
     private Rigidbody rb;
+    private float rotY;
+    private float rotY_prev;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rotY_prev = startAngle;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        h = Input.GetAxis("Horizontal") * speed;
-        v = Input.GetAxis("Vertical") * speed;
+        h = Input.GetAxis(horizontal) * speed;
+        v = Input.GetAxis(vertical) * speed;
         Vector3 vel = rb.velocity;
         vel.x = h;
         vel.z = v;
-        transform.rotation = Quaternion.Euler(0f, Mathf.Atan2(-v, h) * Mathf.Rad2Deg, 0f);
+        rotY = Mathf.Atan2(-v, h) * Mathf.Rad2Deg;
+        rotY = h+v == 0 && rotY_prev != 0 ? rotY_prev : rotY;
+        transform.rotation = Quaternion.Euler(0f, rotY, 0f);
+        rotY_prev = rotY;
         rb.velocity = vel;
     }
 }
